7e6cf5ea1f73f459a417e0fb24a5c365a007c631
Khalid Mohamud <kmohamud1@outlook.com>
2018-10-31 15:21:22 -0700

    starting branch format

--- Hackerrank/Strings/index.js
+++ Hackerrank/Strings/index.js
@@ -1,4 +1,4 @@
-/*  Anagram
+/*     Anagram
 make a function that recieves two strings and and returns the number of characters 
 that are not the found in both strings
   link: https://www.hackerrank.com/challenges/ctci-making-anagrams/problem
@@ -7,20 +7,20 @@ that are not the found in both strings
 function makeAnagram(a, b) {
   let tracker = {};
   let count = 0;
-
+  
   for(let i=0; i<a.length; i++)
-      tracker[a[i]] = ( ++tracker[a[i]] || 1 )
+    tracker[a[i]] = ( ++tracker[a[i]] || 1 )
 
   for(let i=0; i<b.length; i++) {
-      if( !tracker[b[i]] ) continue
-      --tracker[b[i]]
-      count += 2
+    if( !tracker[b[i]] ) continue
+    --tracker[b[i]]
+    count += 2
   }
   return (a.length + b.length) - count
 }
 
 
-/*             Alternating Characters
+/*    Alternating Characters
   Make a function that returns the count of adjacent matching characters
   eg ABABABABAB should return 0
      AAAA should return 3
